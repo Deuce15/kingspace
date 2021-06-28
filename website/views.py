acf5a4960b3285b262823c04c0783ff21a45a8dc
deuce15 <isaiahnyamu@gmail.com>
2021-06-28 09:30:16 +0300

    Adding features

--- website/views.py
+++ website/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 def home(request):
 	return render(request, 'home.html', {})
 
 def contact(request):
-	return render(request, 'contact.html', {})
+	if request.method == "POST":
+		message_name = request.POST['message-name']
+		message_email = request.POST['message-email']
+		message_subject = request.POST['message-subject']
+		message = request.POST['message']
+
+		# send an email
+		send_mail(
+			message_name, # subject
+			message, # message
+			message_email, # from email
+			['i.nyamu5@gmail.com'], # to email
+
+			)
+
+		return render(request, 'contact.html', {'message_name': message_name})
+	else:	
+		return render(request, 'contact.html', {})
 
 def blog(request):
 	return render(request, 'blog.html', {})
